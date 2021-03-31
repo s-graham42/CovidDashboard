c46265bc3c5830cb3b9f8e50a2873ad2ea87a9bb
Bradford-Howe <breaksfiend@gmail.com>
2021-03-31 16:41:57 -0700

    R call update_Now creates one NYT.CSV with cumu and daily data.

--- Brad's_Work/Py_call_R_project/Get_us-states_githubrepo_NYT.R
+++
@@ -1,8 +0,0 @@
-#--- Set working directory to a directory mutually understood with Python script
-setwd('C:\Users\toast\Documents\CodingDojo\python_stack\CovidDashboard\covid_project\csv_files')
-
-#--- Import States data from the NYT github repository into an R object
-init=read.csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv')
-
-#--- Export that R object as a CSV file
-write.csv(init,file="us-states_githubrepo_NYT_1.csv",row.names = FALSE)
