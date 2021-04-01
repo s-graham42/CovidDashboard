7a7b93290227b3ae33520f21fd2b25ce6d261603
Stephen Graham <sgraham42@gmail.com>
2021-04-01 01:42:42 -0700

    cleaning up deprecated function in datasources.py

--- dashboard/datasources.py
+++ dashboard/datasources.py
@@ -86,33 +86,6 @@ class NewYorkTimesStateData(DataSourceCSV):
             os.system("Rscript " + BASE_DIR + "/Brad's_Work/Py_call_R_project/Cumu_To_Daily_NYT.R")
             return pd.read_csv(self.source_file, parse_dates=True)
 
-    # deprecated
-    # def get_new_from_api(self):
-    #                 # Get new csv file from NYTimes Github page
-    #     api_file = pd.read_csv(self.source_url, parse_dates=True)
-        
-    #     max_fips = max(api_file['fips'])
-    #     new_frame = pd.DataFrame()
-    #                 # Calculate Daily Cases and Deaths
-    #     for i in range(1, max_fips + 1):        
-    #         this_state = api_file[api_file['fips'] == i].sort_values(by=['date'])
-    #         if len(this_state) == 0:
-    #             continue
-    #         else:
-    #             this_state['cases_d'] = this_state['cases'].diff()
-    #             this_state['deaths_d'] = this_state['deaths'].diff()
-    #                 # after .diif(), first item in new series is NaN.  Assign value from cumulative data to that item.
-    #             this_state.loc[this_state.index[0], 'cases_d'] = this_state.loc[this_state.index[0], 'cases']
-    #             this_state.loc[this_state.index[0], 'deaths_d'] = this_state.loc[this_state.index[0], 'deaths']
-
-    #                 # append each state in turn to new Data Frame
-    #             new_frame = new_frame.append(this_state, ignore_index = True)
-
-    #                 # cast calculated columns from float64 to int32
-    #     new_frame = new_frame.astype({'cases_d': 'int32', 'deaths_d': 'int32'})
-
-    #     return new_frame
-
                     # get all 'column' entries for a particular state (by fips number)
     def get_all_by_state(self, fips, column):
         this_state = State.objects.get(fips=fips)
@@ -126,7 +99,6 @@ class NewYorkTimesStateData(DataSourceCSV):
         this_data = self.df.loc[(self.df["date"] >= start) & (self.df["date"] <= end) & (self.df["fips"] == fips), ["date", column]].sort_values(by=['date'])
                     # chart generation needs a list of tuples in the format ('date', 'value')
         this_xy = list(this_data.itertuples(index=False, name=None))
-        
                     # return dictionary {state: state name, data: list of date, value tuples
         return {"state": this_state.name, "data": list(this_xy)}
 
