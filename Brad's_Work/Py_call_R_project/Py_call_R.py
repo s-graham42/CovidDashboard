c9d714712e033da51f0040e89685cdefcc2b3870
Bradford-Howe <breaksfiend@gmail.com>
2021-04-14 21:05:18 -0700

    CDC data processing added

--- Brad's_Work/Py_call_R_project/Py_call_R.py
+++ Brad's_Work/Py_call_R_project/Py_call_R.py
@@ -5,24 +5,31 @@ import pandas as pd
 # Path to R script.
 a = "/Users/neo/Drive/Python/WuFlu_Dashboard/WuFlu_Sandbox/WD_Sandbox/NYT_Territory_Data_Processing.R"
 
-# Path to directory in which the produced CSV should be placed.
-b = "/Users/neo/Drive/Python/WuFlu_Dashboard/WuFlu_Sandbox/WD_Sandbox/Test_Folder"
-
-# Path for CSV imporation into a dataframe.
-c = "/Users/neo/Drive/Python/WuFlu_Dashboard/WuFlu_Sandbox/WD_Sandbox/Test_Folder/NYT_States.csv"
+# Path to directory in which the produced CSV should be placed and where it can be retrieved.
+b = "/Users/neo/Drive/Python/WuFlu_Dashboard/WuFlu_Sandbox/WD_Sandbox/Test_Folder/NYT_States.csv"
 
 os.system("Rscript " + a + " " + b)
-NYT_States = pd.read_csv(c)
+NYT_States = pd.read_csv(b)
+
 
 # Path to R script
 a = "/Users/neo/Drive/Python/WuFlu_Dashboard/WuFlu_Sandbox/WD_Sandbox/NYT_US_Data_Processing.R"
 
-# Path to directory in which the produced CSV should be placed.
-b = "/Users/neo/Drive/Python/WuFlu_Dashboard/WuFlu_Sandbox/WD_Sandbox/Test_Folder"
-
-# Path for CSV imporation into a dataframe.
-c = "/Users/neo/Drive/Python/WuFlu_Dashboard/WuFlu_Sandbox/WD_Sandbox/Test_Folder/NYT_US.csv"
+# Path to directory in which the produced CSV should be placed and where it can be retrieved.
+b = "/Users/neo/Drive/Python/WuFlu_Dashboard/WuFlu_Sandbox/WD_Sandbox/Test_Folder/NYT_US.csv"
 
 os.system("Rscript " + a + " " + b)
-NYT_US = pd.read_csv(c)
+NYT_US = pd.read_csv(b)
 
+
+# Path to R script
+a = "/Users/neo/Drive/Python/WuFlu_Dashboard/WuFlu_Sandbox/WD_Sandbox/CDC_Territory_Data_Processing.R"
+
+# Path to directory in which the produced CSV should be placed and where it can be retrieved.
+b = "/Users/neo/Drive/Python/WuFlu_Dashboard/WuFlu_Sandbox/WD_Sandbox/Test_Folder/CDC_States.csv"
+
+# Working directory for current R script.
+c = "/Users/neo/Drive/Python/WuFlu_Dashboard/WuFlu_Sandbox/WD_Sandbox"
+
+os.system("Rscript " + a + " " + b + " " + c)
+NYT_US = pd.read_csv(b)
